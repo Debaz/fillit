8dc8d9e4293b2d1b8803c86e137822f8dcc19aaa
Kantin LESCAUDRON <klescaud@e1r2p3.42.fr>
2015-12-01 11:32:48 +0100

    Start

---
+++ includes/fillit.h
@@ -0,0 +1,8 @@
+/* Inserer ici header 42 */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+
+#endif
---
+++ sources/main.c
@@ -0,0 +1,8 @@
+/* Inserer ici header 42 */
+
+#include "fillit.h"
+
+int	main(int ac, char **av)
+{
+
+}
